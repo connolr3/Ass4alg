686235afadd8977d46378db7077710f8cedbcb6f
connolr3 <connolr3@tcd.ie>
2020-04-02 10:50:43 +0100

    dij begin

--- Ass4alg/src/CompetitionDijkstra.java
+++ Ass4alg/src/CompetitionDijkstra.java
@@ -24,8 +24,9 @@ public class CompetitionDijkstra {
 	public int sc;
 	public int numberEdges;
 	public int vertices;
-	public int[][] edges;
-	public int[] distTo;
+	public double[][] edgeWeights;
+	public double[][] distTo;
+	public int[][]  edgeTo;
 	/**
 	 * @param filename: A filename containing the details of the city road network
 	 * @param sA, sB, sC: speeds for 3 contestants
@@ -42,29 +43,53 @@ public class CompetitionDijkstra {
 			System.out.println(line+i);
 			if (i==0) {
 				vertices = Integer.parseInt(line);
+				edgeWeights = new double[vertices][vertices];	
+				edgeTo = new int[vertices][vertices];
+				distTo = new double[vertices][vertices];
 			}
 			else if (i==1)
 				numberEdges =  Integer.parseInt(line);
 			if(i>1)
 			{
-				double[][] edges = new double[vertices][vertices];
 				String[] splited = line.split("\\s+");
-				edges[Integer.parseInt(splited[0])][Integer.parseInt(splited[1])]=Double.parseDouble(splited[2]);
-				System.out.println("4-0   "+edges[4][0]);
+				edgeWeights[Integer.parseInt(splited[0])][Integer.parseInt(splited[1])]=Double.parseDouble(splited[2]);
+				System.out.println("4-0   "+edgeWeights[4][0]);
 			}
 		}
+		printArray(distTo)
+		// Initialize distTo[s][s] to 0 and distTo[s][v] to infinity for all other vertices v. 
+		for (int rowIndex = 0;rowIndex<vertices;rowIndex++) {
+			for (int columnIndex=0;columnIndex<vertices;columnIndex++) {
+				if(rowIndex==columnIndex) {
+					distTo[rowIndex][columnIndex]=0;
+					edgeTo[rowIndex][columnIndex]=(Integer) null;//the last edge from itself is null
+					edgeWeights[rowIndex][columnIndex]=0;//the distance from vertice f to vertex f is 0
+				}
+				distTo[rowIndex][columnIndex]=10000;
+			}
+		}
+		int source = 0;
+		
 	}
-	double[]  distTo1 = new double[vertices];
-	double[]  edgeTo = new double[vertices];
-
+	
 	public void getSP (int v ) {
 		for (int i = 0;i<distTo.length;i++) {
-			distTo[i]=10000;
+	//		distTo[i]=10000;
 		}
-		distTo[v]=0;
+	//	distTo[v]=0;
 	}
 	private void relaxEdge(int i) {
 	
+	}
+	public void printArray(double[][]array,int rows,int columns) {
+		for(int i = 0; i<rows; i++)
+		{
+		    for(int j = 0; j<columns; j++)
+		    {
+		        System.out.print(array[i][j]);
+		    }
+		    System.out.println();
+		}
 	}
 	/**
 	 * @return int: minimum minutes that will pass before the three contestants can meet
@@ -75,19 +100,6 @@ public class CompetitionDijkstra {
 		return -1;
 	}
 	public static void main (String[]args) throws FileNotFoundException {
-		File comp = new File("tinyEWD.txt");
-		int vertices =0;
-		int edges = 0;
-		Scanner scan = new Scanner(comp);
-		for(int i = 0;i<2;i++) {
-			String line = scan.nextLine();	
-			if (i==0) {
-				vertices = Integer.parseInt(line);	
-			}
-			if (i==1) {
-				edges =  Integer.parseInt(line);	
-			}
-		}
 		CompetitionDijkstra dd= new CompetitionDijkstra("tinyEWD.txt",3,4,5);
 		//printing("tinyEWD.txt");
 
